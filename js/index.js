6b81ffc3291ea0b37106c890bf146a59a546782e
Junaid Rahim <junaidrahim8d@gmail.com>
2020-04-16 12:59:26 +0530

    added navbar

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// JS and jQuery functions for index.html
+
+// Load navbar and add on click handler
+$(function(){
+    $(".nav-container").load("common/navbar.html", () => {
+        document.getElementById('menu-btn').addEventListener('click', () => {
+            let menuItems = document.querySelectorAll('nav ul li')
+
+            menuItems.forEach((item) => {
+                item.classList.toggle("visible")
+            })
+        })
+    })
+});
+    
