dc2ed39c1c4682fe97f3db9983646e08e19af370
Rohan Kumar <rohankmr414@gmail.com>
2020-04-22 21:39:44 +0530

    added popup faces

--- js/teampage.js
+++ js/teampage.js
@@ -10,18 +10,22 @@ $(function () {
     })
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
     $('.tree-container').load('tree.html');
 })
+
+
+$(function () {
+    $(".footer-container").load("footer.html");
+});
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+  })
 
 window.addEventListener('scroll', function () {
     var element = document.querySelector('.tree-container');
     var position = element.getBoundingClientRect();
-
-    // checking whether fully visible
-    if (position.top >= 0 && position.bottom <= window.innerHeight) {
-        console.log('Element is fully visible in screen');
-    }
 
     // checking for partial visibility
     if (position.top < window.innerHeight && position.bottom >= 0) {
@@ -125,8 +129,124 @@ window.addEventListener('scroll', function () {
         }, 800);
     }
 
+
+    // checking whether fully visible
+    if (position.top >= 0 && position.bottom <= window.innerHeight) {
+        console.log('Element is fully visible in screen');
+        setTimeout(function () {
+            document.querySelector('.p1').classList.add('zoomIn');
+        }, 900);
+        setTimeout(function () {
+            document.querySelector('.p1').classList.remove('hide');
+        }, 900);
+        setTimeout(function () {
+            document.querySelector('.p2').classList.add('zoomIn');
+        }, 1800);
+        setTimeout(function () {
+            document.querySelector('.p2').classList.remove('hide');
+        }, 1800);
+        setTimeout(function () {
+            document.querySelector('.p3').classList.add('zoomIn');
+        }, 1400);
+        setTimeout(function () {
+            document.querySelector('.p3').classList.remove('hide');
+        }, 1400);
+        setTimeout(function () {
+            document.querySelector('.p4').classList.add('zoomIn');
+        }, 2400);
+        setTimeout(function () {
+            document.querySelector('.p4').classList.remove('hide');
+        }, 2400);
+        setTimeout(function () {
+            document.querySelector('.p5').classList.add('zoomIn');
+        }, 2600);
+        setTimeout(function () {
+            document.querySelector('.p5').classList.remove('hide');
+        }, 2600);
+        setTimeout(function () {
+            document.querySelector('.p6').classList.add('zoomIn');
+        }, 1900);
+        setTimeout(function () {
+            document.querySelector('.p6').classList.remove('hide');
+        }, 1900);
+        setTimeout(function () {
+            document.querySelector('.p7').classList.add('zoomIn');
+        }, 1300);
+        setTimeout(function () {
+            document.querySelector('.p7').classList.remove('hide');
+        }, 1300);
+        setTimeout(function () {
+            document.querySelector('.p8').classList.add('zoomIn');
+        }, 2000);
+        setTimeout(function () {
+            document.querySelector('.p8').classList.remove('hide');
+        }, 2000);
+        setTimeout(function () {
+            document.querySelector('.p9').classList.add('zoomIn');
+        }, 2500);
+        setTimeout(function () {
+            document.querySelector('.p9').classList.remove('hide');
+        }, 2500);
+        setTimeout(function () {
+            document.querySelector('.p10').classList.add('zoomIn');
+        }, 1500);
+        setTimeout(function () {
+            document.querySelector('.p10').classList.remove('hide');
+        }, 1500);
+        setTimeout(function () {
+            document.querySelector('.p11').classList.add('zoomIn');
+        }, 2100);
+        setTimeout(function () {
+            document.querySelector('.p11').classList.remove('hide');
+        }, 2100);
+        setTimeout(function () {
+            document.querySelector('.p12').classList.add('zoomIn');
+        }, 1200);
+        setTimeout(function () {
+            document.querySelector('.p12').classList.remove('hide');
+        }, 1200);
+        setTimeout(function () {
+            document.querySelector('.p13').classList.add('zoomIn');
+        }, 2300);
+        setTimeout(function () {
+            document.querySelector('.p13').classList.remove('hide');
+        }, 2300);
+        setTimeout(function () {
+            document.querySelector('.p14').classList.add('zoomIn');
+        }, 1600);
+        setTimeout(function () {
+            document.querySelector('.p14').classList.remove('hide');
+        }, 1600);
+        setTimeout(function () {
+            document.querySelector('.p15').classList.add('zoomIn');
+        }, 1100);
+        setTimeout(function () {
+            document.querySelector('.p15').classList.remove('hide');
+        }, 1100);
+        setTimeout(function () {
+            document.querySelector('.p16').classList.add('zoomIn');
+        }, 2200);
+        setTimeout(function () {
+            document.querySelector('.p16').classList.remove('hide');
+        }, 2200);
+        setTimeout(function () {
+            document.querySelector('.p17').classList.add('zoomIn');
+        }, 1700);
+        setTimeout(function () {
+            document.querySelector('.p17').classList.remove('hide');
+        }, 1700);
+        setTimeout(function () {
+            document.querySelector('.p18').classList.add('zoomIn');
+        }, 1000);
+        setTimeout(function () {
+            document.querySelector('.p18').classList.remove('hide');
+        }, 1000);
+
+    }
+
+
     else {
-        
+
     }
 });
 
