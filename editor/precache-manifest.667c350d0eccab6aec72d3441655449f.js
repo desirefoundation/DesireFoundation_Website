3f1605026539df4e7a8805d0ffc0595382623b37
Junaid Rahim <junaidrahim8d@gmail.com>
2021-01-08 12:52:12 +0530

    added editor build

---
+++ editor/precache-manifest.667c350d0eccab6aec72d3441655449f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38ab100e4b36fc325a96124b22fda05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "241f32e17279f32303d8",
+    "url": "/static/css/2.0edca3b5.chunk.css"
+  },
+  {
+    "revision": "241f32e17279f32303d8",
+    "url": "/static/js/2.b924a534.chunk.js"
+  },
+  {
+    "revision": "1ce0cd407133c7d2605caff662305748",
+    "url": "/static/js/2.b924a534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea48ced38d51db8996a",
+    "url": "/static/js/main.c249c5ac.chunk.js"
+  },
+  {
+    "revision": "249c7d12a5f021d4a51a",
+    "url": "/static/js/runtime-main.2d67b12a.js"
+  }
+]);
